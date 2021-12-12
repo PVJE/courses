dd5e75da0db3c99f25e7ee8709a9cfd70db01e79
PVJE <32874158+PVJE@users.noreply.github.com>
2021-12-12 14:47:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-######## RDS ############
+
 resource "aws_db_instance" "db" {
   allocated_storage      = var.allocated_storage
   engine                 = var.engine
@@ -10,19 +10,19 @@ resource "aws_db_instance" "db" {
   vpc_security_group_ids = [aws_security_group.rds-sg.id]
   skip_final_snapshot    = true
 }
-####### EC2 ############
+
 resource "aws_key_pair" "ec2-key" {
   key_name   = "deployer-key"
   public_key = var.public_key
 }
-resource "aws_instance" "myec2" {
+resource "aws_instance" "my_ec2" {
   ami                    = var.ami_id
   instance_type          = var.ec2_instance_type
   vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   key_name               = aws_key_pair.ec2-key.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_s3_profile.name
 }
-####### S3  ############
+ 
 resource "aws_s3_bucket" "my_bucket" {
     bucket        = var.s3_bucket_name
     acl           = "private"
